11d15b2f262a5d790ade5b183db82f23e90d80f6
MeganG2 <59978070+MeganG2@users.noreply.github.com>
2020-04-14 01:49:20 -0500

    im tired

---
+++ Lab10/Lab10.srcs/sources_1/new/calc_lab10.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 04/14/2020 01:49:05 AM
+// Design Name: 
+// Module Name: calc_lab10
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module calc_lab10(
+
+    );
+endmodule
