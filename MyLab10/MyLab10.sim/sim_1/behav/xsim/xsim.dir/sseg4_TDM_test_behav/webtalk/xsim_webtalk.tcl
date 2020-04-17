e88611051e2ae51c278bf4b7a4c08ee315f4c92c
MeganG2 <59978070+MeganG2@users.noreply.github.com>
2020-04-16 19:10:41 -0500

    sometimes i get sad

--- MyLab10/MyLab10.sim/sim_1/behav/xsim/xsim.dir/sseg4_TDM_test_behav/webtalk/xsim_webtalk.tcl
+++ MyLab10/MyLab10.sim/sim_1/behav/xsim/xsim.dir/sseg4_TDM_test_behav/webtalk/xsim_webtalk.tcl
@@ -1,6 +1,6 @@
 webtalk_init -webtalk_dir C:/Users/Gordoncrew/Documents/GitHub/DL202010_Gordon/MyLab10/MyLab10.sim/sim_1/behav/xsim/xsim.dir/sseg4_TDM_test_behav/webtalk/
 webtalk_register_client -client project
-webtalk_add_data -client project -key date_generated -value "Thu Apr 16 14:34:54 2020" -context "software_version_and_target_device"
+webtalk_add_data -client project -key date_generated -value "Thu Apr 16 19:09:48 2020" -context "software_version_and_target_device"
 webtalk_add_data -client project -key product_version -value "XSIM v2018.3 (64-bit)" -context "software_version_and_target_device"
 webtalk_add_data -client project -key build_version -value "2405991" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_platform -value "WIN64" -context "software_version_and_target_device"
@@ -14,7 +14,7 @@ webtalk_add_data -client project -key target_package -value "not_applicable" -co
 webtalk_add_data -client project -key target_speed -value "not_applicable" -context "software_version_and_target_device"
 webtalk_add_data -client project -key random_id -value "01f07688-9949-468e-bb89-d276224a087e" -context "software_version_and_target_device"
 webtalk_add_data -client project -key project_id -value "ccbf37d71ca249e08384ebc5249caec6" -context "software_version_and_target_device"
-webtalk_add_data -client project -key project_iteration -value "82" -context "software_version_and_target_device"
+webtalk_add_data -client project -key project_iteration -value "83" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_name -value "Microsoft Windows 8 or later , 64-bit" -context "user_environment"
 webtalk_add_data -client project -key os_release -value "major release  (build 9200)" -context "user_environment"
 webtalk_add_data -client project -key cpu_name -value "AMD Athlon(tm) II X4 620 Processor" -context "user_environment"
@@ -22,12 +22,11 @@ webtalk_add_data -client project -key cpu_speed -value "2600 MHz" -context "user
 webtalk_add_data -client project -key total_processors -value "1" -context "user_environment"
 webtalk_add_data -client project -key system_ram -value "8.000 GB" -context "user_environment"
 webtalk_register_client -client xsim
-webtalk_add_data -client xsim -key runall -value "true" -context "xsim\\command_line_options"
 webtalk_add_data -client xsim -key Command -value "xsim" -context "xsim\\command_line_options"
 webtalk_add_data -client xsim -key trace_waveform -value "true" -context "xsim\\usage"
-webtalk_add_data -client xsim -key runtime -value "119381980 ns" -context "xsim\\usage"
+webtalk_add_data -client xsim -key runtime -value "1 us" -context "xsim\\usage"
 webtalk_add_data -client xsim -key iteration -value "1" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Time -value "10.47_sec" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Memory -value "14056_KB" -context "xsim\\usage"
-webtalk_transmit -clientid 988965389 -regid "" -xml C:/Users/Gordoncrew/Documents/GitHub/DL202010_Gordon/MyLab10/MyLab10.sim/sim_1/behav/xsim/xsim.dir/sseg4_TDM_test_behav/webtalk/usage_statistics_ext_xsim.xml -html C:/Users/Gordoncrew/Documents/GitHub/DL202010_Gordon/MyLab10/MyLab10.sim/sim_1/behav/xsim/xsim.dir/sseg4_TDM_test_behav/webtalk/usage_statistics_ext_xsim.html -wdm C:/Users/Gordoncrew/Documents/GitHub/DL202010_Gordon/MyLab10/MyLab10.sim/sim_1/behav/xsim/xsim.dir/sseg4_TDM_test_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
+webtalk_add_data -client xsim -key Simulation_Time -value "0.03_sec" -context "xsim\\usage"
+webtalk_add_data -client xsim -key Simulation_Memory -value "5960_KB" -context "xsim\\usage"
+webtalk_transmit -clientid 1627954139 -regid "" -xml C:/Users/Gordoncrew/Documents/GitHub/DL202010_Gordon/MyLab10/MyLab10.sim/sim_1/behav/xsim/xsim.dir/sseg4_TDM_test_behav/webtalk/usage_statistics_ext_xsim.xml -html C:/Users/Gordoncrew/Documents/GitHub/DL202010_Gordon/MyLab10/MyLab10.sim/sim_1/behav/xsim/xsim.dir/sseg4_TDM_test_behav/webtalk/usage_statistics_ext_xsim.html -wdm C:/Users/Gordoncrew/Documents/GitHub/DL202010_Gordon/MyLab10/MyLab10.sim/sim_1/behav/xsim/xsim.dir/sseg4_TDM_test_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
 webtalk_terminate
